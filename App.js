1448006b80df7fa80c3168c57aad58e6b61a822b
Nitinkumar Yadav <126796871+Jackdv67@users.noreply.github.com>
2023-05-23 08:26:58 +0530

    Task2

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import Task1 from './Component/Task1';
+import Task2 from './Component/Task2';
+
+function App(){
+
+  return(
+    <div className='App-header'>
+        {/* <Task1/> */}
+        <Task2/>
+    </div>
+  )
+}
+export default App;
